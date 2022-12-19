6a9b46f1b85b76d0b47d1bd39296259b9f1119e0
siddarth-ba-72 <siddu2002bharat@gmail.com>
2022-12-19 10:51:00 +0530

    pckgs

---
+++ Lab_Programs/Cube/Cube.java
@@ -0,0 +1,13 @@
+package Cube;
+
+public class Cube {
+    int n;
+
+    public Cube(int n) {
+        this.n = n;
+    }
+
+    public int findCube(int n) {
+        return (n * n * n);
+    }
+}
---
+++ Lab_Programs/Square/Sqaure.java
@@ -0,0 +1,13 @@
+package Square;
+
+public class Sqaure {
+    int n;
+
+    public Sqaure() {
+    }
+
+    public int findSqaure(int n) {
+        this.n = n;
+        return (n * n);
+    }
+}
